0957ac5434356ee74b1548c9aa6708e866326ca2
unishift <unishifft@gmail.com>
2017-11-17 18:57:19 +0300

    Improved cd

--- inbuilt.c
+++ inbuilt.c
@@ -1,6 +1,31 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 int cd(char **argv)
 {
-    return chdir(argv[0]);
+    char *path;
+    if (argv[0] == NULL) {
+        path = getenv("HOME");
+    }
+    else if (argv[1] != NULL) {
+        errno = E2BIG; /* Too many arguments */
+        return -1;
+    }
+    else {
+        switch (argv[0][0]) {
+            case '~': 
+                path = (char*)calloc(strlen(getenv("HOME")) + strlen(argv[0] + 1) + 1, sizeof(char));
+                strcpy(path, getenv("HOME"));
+                strcpy(path + strlen(getenv("HOME")), argv[0] + 1);
+                break;
+            default:
+                path = (char*)calloc(strlen(argv[0]) + 1, sizeof(char));
+                strcpy(path, argv[0]);
+        }
+    }
+    int status = chdir(path); 
+    if (argv[0] != NULL) free(path);
+    return status;
 }
